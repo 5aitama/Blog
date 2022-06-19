f868fde11e82937e9d1a868ae6a41dae05f820d8
Admiral サイタマ <alx.sb@icloud.com>
2022-06-19 19:03:56 +0200

    Fix path issue

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: "./docs"
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: '/Blog/',
 })
